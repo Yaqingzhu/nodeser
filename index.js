0267250305d35db06345fc1c05b29e9150386d9c
Yaqingzhu <yaqingzhu@cmail.carleton.ca>
2022-04-10 15:50:20 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+  
+var pool = mysql.createPool({
+  connectionLimit: 5,
+  host: 'database-2.cdvfcxgckpci.us-east-2.rds.amazonaws.com',
+  user: 'admin',
+  password: '12345678',
+  database: 'android'
+});
+
+// New app using express module
+const app = express();
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+  
+app.get("/top", function(req, res) {
+  var id = req.body.id;
+  pool.getConnection(function(err, connection){
+    let query = "SELECT * FROM scores WHERE id = ? ORDER BY score DESC LIMIT 5 ";
+    let todo = [id];
+    connection.query(query, todo, (err, results, fields) => {
+      if (err) {
+        return console.error(err.message);
+      }
+
+      const converted = JSON.parse(JSON.stringify(results))
+
+      const result = converted.map((i)=>{
+        return i.score;
+      });
+
+      res.send(result);
+    });
+  })
+});
+
+app.get("/topg", function(req, res) {
+  var id = req.body.id;
+  pool.getConnection(function(err, connection){
+    let query = "SELECT * FROM scores ORDER BY score DESC LIMIT 5 ";
+    let todo = [id];
+    connection.query(query, todo, (err, results, fields) => {
+      if (err) {
+        return console.error(err.message);
+      }
+
+      const converted = JSON.parse(JSON.stringify(results))
+
+      const result = converted.map((i)=>{
+        return i.score;
+      });
+
+      res.send(result);
+    });
+  })
+});
+  
+app.post("/score", function(req, res) {
+  var id = req.body.id;
+  var score = Number(req.body.num2);
+    
+  pool.getConnection(function(err, connection){
+    let query = "INSERT INTO scores(id, score) VALUES (?,?)";
+    let todo = [id, score];
+    connection.query(query, todo, (err, results, fields) => {
+      if (err) {
+        return console.error(err.message);
+      }
+    });
+  })
+    
+  res.send("inserted");
+});
+  
+app.listen(3000, function(){
+  console.log("server is running on port 3000");
+})
