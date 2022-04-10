74fcb635e2034b42e63547170b0c2fa9c5a02070
Yaqingzhu <yaqingzhu@cmail.carleton.ca>
2022-04-10 18:09:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,27 @@ app.get("/top", function(req, res) {
   })
 });
 
+app.get("/image", function(req, res) {
+  var id = req.query.id;
+  pool.getConnection(function(err, connection){
+    let query = "SELECT * FROM images WHERE id = ? ORDER BY ind DESC LIMIT 1";
+    let todo = [id];
+    connection.query(query, todo, (err, results, fields) => {
+      if (err) {
+        return console.error(err.message);
+      }
+
+      const converted = JSON.parse(JSON.stringify(results))
+
+      const result = converted.map((i)=>{
+        return i.uri;
+      });
+
+      res.send(result);
+    });
+  })
+});
+
 app.get("/topg", function(req, res) {
   pool.getConnection(function(err, connection){
     let query = "SELECT * FROM scores ORDER BY score DESC LIMIT 5 ";
@@ -64,6 +85,23 @@ app.post("/score", function(req, res) {
   pool.getConnection(function(err, connection){
     let query = "INSERT INTO scores(id, score) VALUES (?,?)";
     let todo = [id, score];
+    connection.query(query, todo, (err, results, fields) => {
+      if (err) {
+        return console.error(err.message);
+      }
+    });
+  })
+    
+  res.send("inserted");
+});
+
+app.post("/images", function(req, res) {
+  var id = req.body.id;
+  var uri = req.body.uri;
+    
+  pool.getConnection(function(err, connection){
+    let query = "INSERT INTO images(id, uri) VALUES (?,?)";
+    let todo = [id, uri];
     connection.query(query, todo, (err, results, fields) => {
       if (err) {
         return console.error(err.message);
